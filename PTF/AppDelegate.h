12784082f0a7b1f936280915319bc1983ae9b074
Asaph Yuan <asaphy@bu.edu>
2014-03-21 21:50:53 -0400

    Initial Commit

---
+++ PTF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTF
+//
+//  Created by Asaph Yuan on 3/21/14.
+//  Copyright (c) 2014 Global App Initiative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

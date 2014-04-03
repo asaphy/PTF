39cd16e3ed68b1da9326cb0ddf6217c5183146b0
B1ueSky <programmer.lty@gmail.com>
2014-04-03 01:16:18 -0400

    add view container and three segmented control for choosing from driver, food provider and chaperone.

--- PTF/CalendarViewController.h
+++ PTF/CalendarViewController.h
@@ -2,13 +2,12 @@
 //  CalendarViewController.h
 //  PTF
 //
-//  Created by Asaph Yuan on 3/21/14.
+//  Created by Jeremy_Luo on 4/3/14.
 //  Copyright (c) 2014 Global App Initiative. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <PDTSimpleCalendar/PDTSimpleCalendar.h>
 
-@interface CalendarViewController : PDTSimpleCalendarViewController
+@interface CalendarViewController : UIViewController
 
 @end
--- PTF/CalendarViewController.m
+++ PTF/CalendarViewController.m
@@ -1,31 +1,14 @@
 //
 //  CalendarViewController.m
-//  PTF //
-//  Created by Asaph Yuan on 3/21/14.
+//  PTF
+//
+//  Created by Jeremy_Luo on 4/3/14.
 //  Copyright (c) 2014 Global App Initiative. All rights reserved.
 //
-//Calendar:
-//Copyright 2013 Jive Software, Inc.
-//
-//Licensed under the Apache License, Version 2.0 (the "License");
-//you may not use this file except in compliance with the License.
-//You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.0
-//
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
 
 #import "CalendarViewController.h"
-#import <PDTSimpleCalendar/PDTSimpleCalendar.h>
-#import <Parse/Parse.h>
 
-@interface CalendarViewController () <PDTSimpleCalendarViewDelegate>
-
-@property (nonatomic, strong) NSArray *customDates;
+@interface CalendarViewController ()
 
 @end
 
@@ -43,21 +26,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
-    dateFormatter.dateFormat = @"dd/MM/yyyy";
-    _customDates = @[[dateFormatter dateFromString:@"01/05/2014"], [dateFormatter dateFromString:@"01/06/2014"], [dateFormatter dateFromString:@"01/07/2014"]];
-    
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)logOut:(id)sender {
-    [PFUser logOut];
-    [self.navigationController.navigationController popToRootViewControllerAnimated:YES];
 }
 
 /*

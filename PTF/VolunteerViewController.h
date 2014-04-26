7b4c441972649e166411b8845921e96755f8321c
Asaph Yuan <asaphy@bu.edu>
2014-04-26 01:31:16 -0400

    volunteer page updated

--- PTF/VolunteerViewController.h
+++ PTF/VolunteerViewController.h
@@ -1,25 +1,45 @@
 //
+
 //  VolunteerViewController.h
+
 //  PTF
+
 //
+
 //  Created by Jeremy_Luo on 4/3/14.
+
 //  Copyright (c) 2014 Global App Initiative. All rights reserved.
+
 //
+
+
 
 #import <UIKit/UIKit.h>
+
 #import <Parse/Parse.h>
-@interface VolunteerViewController : UIViewController <UITextFieldDelegate> 
+
+@interface VolunteerViewController : UIViewController <UITextFieldDelegate,UIPickerViewDataSource,UIPickerViewDelegate>
+
+
+
 @property (strong, nonatomic) IBOutlet UIPickerView *locationPicker;
+
 @property (strong, nonatomic) IBOutlet UIPickerView *timePicker;
+
 @property (strong, nonatomic) NSArray *array;
+
 @property (strong, nonatomic) NSString *dateFromCal;
+
 @property (strong, nonatomic) NSArray *startTimeArray;
+
 @property (strong, nonatomic)  UITextField *contactName;
+
 @property (strong, nonatomic)  UITextField *contactNumber;
+
 @property (weak, nonatomic) IBOutlet UIButton *addEventButton;
-@property (weak, nonatomic) IBOutlet UITextField *contactName1;
-@property (weak, nonatomic) IBOutlet UITextField *contactNumber1;
+
+
 
 - (IBAction)addEvent:(id)sender;
-@end
 
+@end
